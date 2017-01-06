91ee701c2d9f7fce43e04d8eaff3557863fd136b
Joshua Sierles <joshua@diluvia.net>
2017-01-06 19:07:08 +0100

    fix push script

--- push.sh
+++ push.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 #
-TAG=$(git branch)-$(git rev-parse --short HEAD)
+TAG=$(git rev-parse --abbrev-ref HEAD)-$(git rev-parse --short HEAD)
 echo "Tagging as $TAG"
-git tag $(git branch)-$(git rev-parse --short HEAD)
+git tag $TAG
 echo "Pushing to trigger automated build"
 git push --tags
+git push
 echo "Check builds at https://hub.docker.com/r/nextjournal/julia/builds/"
