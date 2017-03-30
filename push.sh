b1799ffe958982a94cd94d6e17e07baa5e7ddd85
Joshua Sierles <joshua@diluvia.net>
2017-03-30 17:14:05 +0200

    build and push the docker image to Google Container Registry

--- push.sh
+++ push.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 #
 TAG=$(git rev-parse --abbrev-ref HEAD)-$(git rev-parse --short HEAD)
-echo "Tagging as $TAG"
+GCR_IMAGE=eu.gcr.io/nextjournal-com/julia:$TAG
+docker build -t nextjournal/julia:$TAG .
+docker tag nextjournal/julia:$TAG $GCR_IMAGE
+echo "Tagging $TAG"
 git tag $TAG
-echo "Pushing to trigger automated build"
+echo "Pushing to $GCR_IMAGE..."
+docker push $GCR_IMAGE
+echo "Pushing to Github, which triggers an automated build for nextjournal/julia:$TAG"
 git push --tags
 git push
-echo "Check builds at these URLs"
-echo "https://hub.docker.com/r/nextjournal/julia/builds/"
